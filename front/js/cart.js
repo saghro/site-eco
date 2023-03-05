a3faa0a1d058796c6e8c282dd9728a9e0f77f53d
Saghroayoub <ayoubsaghro27@gmail.com>
2023-03-05 22:31:33 +0100

    add js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Initialisation du local storage
 let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 console.table(produitLocalStorage);
@@ -93,17 +92,3 @@ for (let produit in produitLocalStorage){
 }}
 getCart();
 
-=======
-async function Ajouter() {
-    const ctn = document.querySelector('#items')
-
-
-    fetch("http://localhost:3000/api/products")
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-
-            }
-            Ajouter()
->>>>>>> 30e547b7b011598656287d1d682b10a3b6fa9913
