598fc722c39c56dad15d54db48cc6de0758ff122
HossameTor <hossametorchi07@gmail.com>
2023-03-06 16:52:05 +0100

    add qnt

--- front/js/product.js
+++ front/js/product.js
@@ -4,7 +4,7 @@ var idProduct = url.searchParams.get("id");
 console.log(idProduct);
 let article = "";
 
-const colorPicked = document. querySelector("#colors");
+const colorPicked = document.querySelector("#colors");
 const quantityPicked = document.querySelector("#quantity");
 
 getArticle();
@@ -12,24 +12,24 @@ getArticle();
 // Récupération des articles de l'API
 function getArticle() {
     fetch("http://localhost:3000/api/products/" + idProduct)
-    .then((res) => {
-        return res.json();
-    })
+        .then((res) => {
+            return res.json();
+        })
 
     // Répartition des données de l'API dans le DOM
-    .then(async function (resultatAPI) {
-        article = await resultatAPI;
-        console.table(article);
-        if (article){
-            getPost(article);
-        }
-    })
-    .catch((error) => {
-        console.log("Erreur de la requête API");
-    })
+    .then(async function(resultatAPI) {
+            article = await resultatAPI;
+            console.table(article);
+            if (article) {
+                getPost(article);
+            }
+        })
+        .catch((error) => {
+            console.log("Erreur de la requête API");
+        })
 }
-    
-function getPost(article){
+
+function getPost(article) {
     // Insertion de l'image
     let productImg = document.createElement("img");
     document.querySelector(".item__img").appendChild(productImg);
@@ -49,7 +49,7 @@ function getPost(article){
     productDescription.innerHTML = article.description;
 
     // Insertion des options de couleurs
-    for (let colors of article.colors){
+    for (let colors of article.colors) {
         console.table(colors);
         let productColors = document.createElement("option");
         document.querySelector("#colors").appendChild(productColors);
@@ -64,66 +64,64 @@ function addToCart(article) {
     const btn_envoyerPanier = document.querySelector("#addToCart");
 
     //Ecouter le panier avec 2 conditions couleur non nulle et quantité entre 1 et 100
-    btn_envoyerPanier.addEventListener("click", (event)=>{
-        if (quantityPicked.value > 0 && quantityPicked.value <=100 && quantityPicked.value != 0){
+    btn_envoyerPanier.addEventListener("click", (event) => {
+        if (quantityPicked.value > 0 && quantityPicked.value <= 100 && quantityPicked.value != 0) {
 
-    //Recupération du choix de la couleur
-    let choixCouleur = colorPicked.value;
-                
-    //Recupération du choix de la quantité
-    let choixQuantite = quantityPicked.value;
+            //Recupération du choix de la couleur
+            let choixCouleur = colorPicked.value;
 
-    //Récupération des options de l'article à ajouter au panier
-    let optionsProduit = {
-        idProduit: idProduct,
-        couleurProduit: choixCouleur,
-        quantiteProduit: Number(choixQuantite),
-        nomProduit: article.name,
-        prixProduit: article.price,
-        descriptionProduit: article.description,
-        imgProduit: article.imageUrl,
-        altImgProduit: article.altTxt
-    };
+            //Recupération du choix de la quantité
+            let choixQuantite = quantityPicked.value;
 
-    //Initialisation du local storage
-    let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+            //Récupération des options de l'article à ajouter au panier
+            let optionsProduit = {
+                idProduit: idProduct,
+                couleurProduit: choixCouleur,
+                quantiteProduit: Number(choixQuantite),
+                nomProduit: article.name,
+                prixProduit: article.price,
+                descriptionProduit: article.description,
+                imgProduit: article.imageUrl,
+                altImgProduit: article.altTxt
+            };
 
-    //fenêtre pop-up
-    const popupConfirmation =() =>{
-        if(window.confirm(`Votre commande de ${choixQuantite} ${article.name} ${choixCouleur} est ajoutée au panier Pour consulter votre panier, cliquez sur OK`)){
-            window.location.href ="cart.html";
+            //Initialisation du local storage
+            let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+
+            //fenêtre pop-up
+            const popupConfirmation = () => {
+                if (window.confirm(`Votre commande de ${choixQuantite} ${article.name} ${choixCouleur} est ajoutée au panier Pour consulter votre panier, cliquez sur OK`)) {
+                    window.location.href = "cart.html";
+                }
+            }
+
+            // Si le panier comporte déjà au moins 1 article
+            if (produitLocalStorage) {
+                const resultFind = produitLocalStorage.find(
+                    (el) => el.idProduit === idProduct && el.couleurProduit === choixCouleur);
+                //Si le produit commandé est déjà dans le panier
+                if (resultFind) {
+                    let newQuantite =
+                        parseInt(optionsProduit.quantiteProduit) + parseInt(resultFind.quantiteProduit);
+                    resultFind.quantiteProduit = newQuantite;
+                    localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+                    console.table(produitLocalStorage);
+                    popupConfirmation();
+                    //Si le produit commandé n'est pas dans le panier
+                } else {
+                    produitLocalStorage.push(optionsProduit);
+                    localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+                    console.table(produitLocalStorage);
+                    popupConfirmation();
+                }
+                //Si le panier est vide
+            } else {
+                produitLocalStorage = [];
+                produitLocalStorage.push(optionsProduit);
+                localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+                console.table(produitLocalStorage);
+                popupConfirmation();
+            }
         }
-    }
-
-    //Importation dans le local storage
-
-    // Si le panier comporte déjà au moins 1 article
-
-    if (produitLocalStorage) {
-    const resultFind = produitLocalStorage.find(
-        (el) => el.idProduit === idProduct && el.couleurProduit === choixCouleur);
-        //Si le produit commandé est déjà dans le panier
-        if (resultFind) {
-            let newQuantite =
-            parseInt(optionsProduit.quantiteProduit) + parseInt(resultFind.quantiteProduit);
-            resultFind.quantiteProduit = newQuantite;
-            localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
-            console.table(produitLocalStorage);
-            popupConfirmation();
-        //Si le produit commandé n'est pas dans le panier
-        } else {
-            produitLocalStorage.push(optionsProduit);
-            localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
-            console.table(produitLocalStorage);
-            popupConfirmation();
-        }
-    //Si le panier est vide
-    } else {
-        produitLocalStorage =[];
-        produitLocalStorage.push(optionsProduit);
-        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
-        console.table(produitLocalStorage);
-        popupConfirmation();
-    }}
     });
 }
--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-fillSection();
+afficher();
 
 // Récupération des articles de l'API
 async function getArticles() {
@@ -6,43 +6,43 @@ async function getArticles() {
     return await articlesCatch.json();
 }
 
-    // Répartition des données de l'API dans le DOM
-async function fillSection() {
-    var result = await getArticles ()
-    .then(function (resultatAPI){
-        const articles = resultatAPI;
-        console.table(articles);
-        for (let article in articles) {
+// Répartition des données de l'API dans le DOM
+async function afficher() {
+    var res = await getArticles()
+        .then(function(resultatAPI) {
+            const articles = resultatAPI;
+            console.table(articles);
+            for (let el in articles) {
 
-            // Insertion de l'élément "a"
-            let productLink = document.createElement("a");
-            document.querySelector(".items").appendChild(productLink);
-            productLink.href = `product.html?id=${resultatAPI[article]._id}`;
+                // Insertion de l'élément "a"
+                let productLink = document.createElement("a");
+                document.querySelector(".items").appendChild(productLink);
+                productLink.href = `product.html?id=${resultatAPI[el]._id}`;
 
-            // Insertion de l'élément "article"
-            let productArticle = document.createElement("article");
-            productLink.appendChild(productArticle);
+                // Insertion de l'élément "article"
+                let productArticle = document.createElement("article");
+                productLink.appendChild(productArticle);
 
-            // Insertion de l'image
-            let productImg = document.createElement("img");
-            productArticle.appendChild(productImg);
-            productImg.src = resultatAPI[article].imageUrl;
-            productImg.alt = resultatAPI[article].altTxt;
+                // Insertion de l'image
+                let imag = document.createElement("img");
+                productArticle.appendChild(imag);
+                imag.src = resultatAPI[el].imageUrl;
+                imag.alt = resultatAPI[el].altTxt;
 
-            // Insertion du titre "h3"
-            let productName = document.createElement("h3");
-            productArticle.appendChild(productName);
-            productName.classList.add("productName");
-            productName.innerHTML = resultatAPI[article].name;
+                // Insertion du titre "h3"
+                let name = document.createElement("h3");
+                productArticle.appendChild(name);
+                name.classList.add("productName");
+                name.innerHTML = resultatAPI[el].name;
 
-            // Insertion de la description "p"
-            let productDescription = document.createElement("p");
-            productArticle.appendChild(productDescription);
-            productDescription.classList.add("productName");
-            productDescription.innerHTML = resultatAPI[article].description;
-        }
-    })
-    .catch (function(error){
-        return error;
-    });
+                // Insertion de la description "p"
+                let description = document.createElement("p");
+                productArticle.appendChild(description);
+                description.classList.add("productName");
+                description.innerHTML = resultatAPI[el].description;
+            }
+        })
+        .catch(function(error) {
+            return error;
+        });
 }
